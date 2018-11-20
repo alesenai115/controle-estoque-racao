d4020807ffd5eebbfbd7177c6a063aba16ce434f
Alexandre Monteiro <ale.tisenai@gmail.com>
2018-11-20 19:44:18 -0200

    Alexandre - começando a tela inicial

---
+++ controle-estoque-racao/main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+app.on('ready', ()=> {
+    const mainWindow = new BrowserWindow({
+    	width: 1000,
+    	height: 800,
+    	minWidth: 1000,
+    	minHeight: 800
+    });        
+    Menu.setApplicationMenu(null)
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
